fc97149a66f6e3093e537dce83f2985234e247a1
Suiteprecure <huiye@ualberta.ca>
2021-10-26 10:41:22 +0800

    might fix

--- index.js
+++ index.js
@@ -1,106 +1,114 @@
-let screenText = [];
-let firstNumber;
-let secondNumber;
-let sol;
-let operator;
-let operatorList = ['+','-', 'x','÷'];
+let screenText = [0];
+let firstNumber = Number.NEGATIVE_INFINITY;
+let secondNumber = Number.NEGATIVE_INFINITY;
+let solution = Number.NEGATIVE_INFINITY;
+let operator = null;
+let operatorList = ['+','-', 'x','÷','←', 'c', '='];
 
-const pannel = document.querySelector('.pannel');
+const panel = document.querySelector('.panel');
 const screen = document.querySelector('.num');
 
-function inputHandler(text) {
-    // maybe prevent multiple events
-    if (text.length > 1 || text.length === 0) {
-        return;
-    }
-    // prevent input operator only
-    if (!screen.innerText.length && ~operatorList.indexOf(text)) {
-        console.log('prevent input operator only');
-        return;
-    }
+function rerender() {
+    screen.innerText = screenText.join('');
+}
 
-    if (text === '←') {
-        console.log('backspace');
-        screenText.pop();
-        screen.innerText = screenText.join('');
-    }
+function renderSolution() {
+    firstNumber = solution;
+    screen.innerText = String(firstNumber);
+}
 
-    // reset all
-    if (text === 'c') {
-        console.log('c');
-        screenText.length = 0;
-        screen.innerText = 0;
-        firstNumber = secondNumber = operator = sol = null;
-        return;
-    } 
-    if (text === '=') {
-        console.log('=');
-        secondNumber = Number(screen.innerText);
-        if (firstNumber && secondNumber) {
-            calcSol();
-            return;
-        }   
-    }
-    if (/^\d+$/.test(text)) {
-        // prevent 0 start
-        if (screen.innerText == 0 && text == 0) {
-            return;
-        }
-        screenText.push(text);
-        console.log(screenText.join(''))
-        screen.innerText = screenText.join('');
-        // if (operator) {
-        //     secondNumber = Number(screenText.join(''));
-        //     calcSol();
-        //     return;
-        // } else {
-        //     screenText.push(text);
-        //     console.log(screenText.join(''))
-        //     screen.innerText = screenText.join('');
-        // }
+function reset() {
+    screenText = [0];
+    firstNumber = Number.NEGATIVE_INFINITY;
+    secondNumber = Number.NEGATIVE_INFINITY;
+    operator = null;
+}
+
+function handleNumber(number) {
+    let num = Number(number);
+    if (screenText[0] == 0) {
+        screenText[0] = num;
     } else {
-        if (operator) {
-            secondNumber = Number(screenText.join(''));
-            calcSol();
-            return;
-        } else {
-            firstNumber = Number(screen.innerText);
-            screenText.length = 0;
-            screen.innerText = 0;
-            handleOperator(text);
-            return;
-        }
+        screenText.push(num);
+    } 
+    rerender();
+}
+
+function flushOperator() {
+    solution = Number.NEGATIVE_INFINITY;
+    switch (operator) {
+        case '+':
+            // console.log('flush + ');
+            solution = Number(firstNumber) + Number(secondNumber);
+            reset();
+            renderSolution();
+            break;
+        case '-':
+            // console.log('flush - ');
+            solution = Number(firstNumber) - Number(secondNumber);
+            reset();
+            renderSolution();
+            break;
+        case 'x':
+            // console.log('flush x ');
+            solution = Number(firstNumber) * Number(secondNumber);
+            reset();
+            renderSolution();
+          break;
+        case '÷':
+            // console.log('flush / ');
+            solution = Number(firstNumber) / Number(secondNumber);
+            reset();
+            renderSolution();
+            break;
+        default:
+          console.log(`wrong default.`, operator);
+      }
+}
+
+function handleMath() {   
+    if (firstNumber === Number.NEGATIVE_INFINITY) {
+        firstNumber = screenText.join('');
+        rerender();
+        screenText = [0];
+    } else if (secondNumber === Number.NEGATIVE_INFINITY && screenText.join('') === screen.innerText) {
+        secondNumber = screenText.join('');
+        flushOperator();
     }
-    return;
 }
 
 function handleOperator(op) {
-    if (op === '÷') {
-        operator = '/';
+    if (op === 'c') {
+        reset();
+        rerender();
+    } else if (op === '←') {
+        if (screenText[0] !== 0) {
+            screenText.pop();
+            rerender();
+        } 
+        if (screenText.length === 0) {
+            screenText = [0];
+            rerender();
+        }
+    } else if (op === '='){
+        secondNumber = screenText.join('');
+        flushOperator();
     } else {
+        handleMath();
         operator = op;
     }
 }
 
-function calcSol() {
-    if (operator === '+') {
-        sol = firstNumber + secondNumber;
+function inputHandler(text) {
+    if (~operatorList.indexOf(text)){
+        handleOperator(text);
+    } else {
+        handleNumber(text);
     }
-    if (operator === '-') {
-        sol = firstNumber - secondNumber;
-    }
-    if (operator === 'x') {
-        sol = firstNumber * secondNumber;
-    }
-    if (operator === '/') {
-        sol = firstNumber / secondNumber;
-    }
-
-    secondNumber = operator = null;
-    firstNumber = sol;
-    screen.innerText = sol;
-    sol = null;
 }
 
-pannel.addEventListener('click', function(event){inputHandler(event.target.innerText)});
+function printStatus() {
+    console.log(`current status: first: ${firstNumber}, second: ${secondNumber}, operator ${operator}, screenbuffer: ${screenText}`);
+}
+panel.addEventListener('click', function(event){inputHandler(event.target.innerText)});
 
